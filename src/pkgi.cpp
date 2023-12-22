d365cf13f33cda3664d90759a565b8f0b005fb0a
Alexander Mironov <goupriver@gmail.com>
2023-12-22 21:25:57 +0700

    фцв

--- src/pkgi.cpp
+++ src/pkgi.cpp
@@ -705,7 +705,7 @@ void pkgi_do_main(Downloader& downloader, pkgi_input* input)
                     10,
                     43,
                     8,
-                    VITA_HEIGHT - 104,
+                    VITA_HEIGHT - 108,
                     PKGI_COLOR_SCROLL_BAR_BACKGROUND);
     
     if (db_count != 0)
