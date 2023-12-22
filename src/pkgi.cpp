71e008d62581aa05acd6fd72f9bd3899fec9ee03
Alexander Mironov <goupriver@gmail.com>
2023-12-23 01:18:23 +0700

    awd

--- src/pkgi.cpp
+++ src/pkgi.cpp
@@ -590,7 +590,7 @@ void pkgi_do_main(Downloader& downloader, pkgi_input* input)
             pkgi_draw_rect(
                     0 + 26,
                     y + 3,
-                    VITA_WIDTH - 33,
+                    VITA_WIDTH - 34,
                     font_height + PKGI_MAIN_ROW_PADDING - 4,
                     PKGI_COLOR_SELECTED_BACKGROUND);
         }
