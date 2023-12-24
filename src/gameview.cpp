cf4ded137825df67d7b442073c3119483f70a6e1
Alexander Mironov <goupriver@gmail.com>
2023-12-25 01:00:59 +0700

    awd

--- src/gameview.cpp
+++ src/gameview.cpp
@@ -167,8 +167,11 @@ void GameView::render()
         pkgi_texture tex2 = pkgi_load_png(coverdisk);
         // vita2d_texture *tex2 = vita2d_load_PNG_file("ux0:pkgj/cover/coverdisk.png");
         ImGui::SetCursorPos(ImVec2(tex_x, tex_y));
-        ImGui::Image(tex2, ImVec2(50.0f, 50.0f));
-        vita2d_fini();
+
+        vita2d_wait_rendering_done();
+        vita2d_free_texture(tex2);
+        // ImGui::Image(tex2, ImVec2(50.0f, 50.0f));
+        // vita2d_fini();
         // vita2d_free_texture(текс); -> сбой
         // _texture = vita2d_load_PNG_file("ux0:pkgj/cover/coverdisk.png".c_str());
 
