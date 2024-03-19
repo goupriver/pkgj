020693f0ad691c11c6011fee9903f7553435cf9d
Alexander Mironov <goupriver@gmail.com>
2024-03-20 00:04:31 +0700

    1.0 + ImGuiCol_BorderShadow

--- src/pkgi.cpp
+++ src/pkgi.cpp
@@ -1669,7 +1669,7 @@ int main()
         // style.Colors[ImGuiCol_ModalWindowDarkening] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
         // style.PopupBorderSize = 0.1f;
         // style.WindowBorderSize = 5.0f;
-        style.FrameBorderSize = 0.5f;
+        style.FrameBorderSize = 1.0f;
 
         init_imgui();
 
