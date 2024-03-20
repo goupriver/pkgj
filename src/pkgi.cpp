a0405173d271dfa62861ed137915031d279551b1
Alexander Mironov <goupriver@gmail.com>
2024-03-20 15:44:14 +0700

    9

--- src/pkgi.cpp
+++ src/pkgi.cpp
@@ -1746,11 +1746,12 @@ int main()
 
             pkgi_draw_text_with_size(VITA_WIDTH - 64, -2, 0.750f, PKGI_COLOR_TEXT, fmt::format("%").c_str());
             
-            pkgi_draw_text_with_size(VITA_WIDTH - 67 - pkgi_text_width(fmt::format("{}", pkgi_bettery_get_level()).c_str()),
+            pkgi_draw_text_with_size(VITA_WIDTH - 57 - pkgi_text_width(fmt::format("{}", pkgi_bettery_get_level()).c_str()),
              -2, 
              0.750f, 
              PKGI_COLOR_TEXT, 
-             fmt::format("{}", pkgi_bettery_get_level()).c_str()
+            //  fmt::format("{}", pkgi_bettery_get_level()).c_str()
+             fmt::format("{}", "9").c_str()
             );
 
             // pkgi_draw_text_with_size(
